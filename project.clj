565c7e097d7cff93a70df0d7180497f816e959c1
IJK <isaac@isaackarth.com>
2012-10-26 09:18:36 -0500

    First commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject emotion "0.1.0-SNAPSHOT"
+  :description "Emotional game prototype"
+  :url "http://isaackarth.com/games"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-swank "1.4.4"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+				 [quil "1.6.0"]])
---
+++ src/emotion/dynamic.clj
@@ -0,0 +1,18 @@
+(ns emotion.dynamic
+  (:use quil.core))
+
+(defn setup []
+  (smooth)
+  (frame-rate 1)
+  (background 200))
+
+(defn draw []
+  (stroke (random 255) (random 255) (random 255))
+  (stroke-weight (random 10))
+  (fill (random 255))
+
+  (let [diam (random 100)
+        x    (random (width))
+        y    (random (height))]
+    (ellipse x y diam diam)))
+      
